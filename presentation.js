fee8815d4284ba6ed92df6bb590db03f29b93849
raghad alnserat <raghadalnusirat2508@gmail.com>
2025-03-17 15:56:59 +0300

    Update presentation.js

--- presentation.js
+++ presentation.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res) => {
         res.status(400).json({ message: err.message });
     }
 });
-
+///testing 
 router.post('/activate', async (req, res) => {
     try {
         const { email, activationCode } = req.body;
